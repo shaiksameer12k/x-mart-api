81d2c8301eb32b29f8bbcef38fb377c778214bde
shaiksameer12k <shaiksameer12k@gmail.com>
2025-12-04 17:54:46 +0530

    addes await for pool

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ import cors from "cors";
 import { userRouter } from "./routes/user-router.js";
 import { connectDB } from "./config/DB_Connection.js";
 
-export const pool = connectDB();
+export const pool = await connectDB();
+
 const app = express();
 const PORT = 4000;
 app.use(express.json());
@@ -11,9 +12,9 @@ app.use(cors());
 
 app.use("/api/v1", userRouter);
 
-app.get("/",(req,res)=>{
-   res.send("Happy Development")
-})
+app.get("/", (req, res) => {
+  res.send("Happy Development");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT : http://localhost:${PORT}`);
