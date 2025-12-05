97db6488b20b2983df561f33bc7a0d1c9ab4e982
shaiksameer12k <shaiksameer12k@gmail.com>
2025-12-05 10:41:38 +0530

    test / link

--- server.js
+++ server.js
@@ -3,19 +3,16 @@ import cors from "cors";
 import { userRouter } from "./routes/user-router.js";
 import { connectDB } from "./config/DB_Connection.js";
 
-await connectDB()
+await connectDB();
 const app = express();
 const PORT = 4000;
 app.use(express.json());
 app.use(cors());
 
-
 app.use("/api/v1", userRouter);
 
 app.get("/", async (req, res) => {
-  const pool = await connectDB();
-  const users = await pool.request().query("select * from users");
-  res.json({data: users , message:"Happy Develpoment"});
+  res.json({ message: "Happy Develpoment" });
 });
 
 app.listen(PORT, () => {
