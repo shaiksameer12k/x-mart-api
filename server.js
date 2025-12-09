07ba99798541f64e856687757b90163725f3af96
shaiksameer12k <shaiksameer12k@gmail.com>
2025-12-09 20:13:56 +0530

    update

--- server.js
+++ server.js
@@ -10,11 +10,44 @@ const app = express();
 const PORT = 4000;
 
 // Swagger Endpoint
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get("/api-docs", (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>X-Mart API Docs</title>
+
+      <link rel="stylesheet"
+            href="https://unpkg.com/swagger-ui-dist/swagger-ui.css">
+    </head>
+
+    <body>
+      <div id="swagger-ui"></div>
+
+      <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js"></script>
+      <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-standalone-preset.js"></script>
+
+      <script>
+        window.onload = () => {
+          SwaggerUIBundle({
+            url: "/swagger.json",
+            dom_id: "#swagger-ui",
+          });
+        };
+      </script>
+
+    </body>
+    </html>
+  `);
+});
+
+app.get("/swagger.json", (req, res) => {
+  res.json(swaggerSpec);
+});
 
 app.use(express.json());
 app.use(cors());
-
 
 app.get("/", async (req, res) => {
   res.json({ message: "Happy Development" });
@@ -25,7 +58,6 @@ app.use("/api/v1", authRouter);
 app.use("/api/v1", userRouter);
 app.use("/api/v1", productsRouter);
 app.use("/api/v1", ordersRouter);
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT : http://localhost:${PORT}`);
